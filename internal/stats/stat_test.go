553daa7f0a3539ec03ea448b8c2ef4c4c1657f3f
lim-bo <artemshap66@gmail.com>
2025-07-05 14:43:30 +0300

    refactor: simplified logic for new clickhouse engine

--- internal/stats/stat_test.go
+++ internal/stats/stat_test.go
@@ -1,12 +1,19 @@
 package stats_test
 
 import (
+	"context"
+	"database/sql"
 	"errors"
+	"regexp"
 	"testing"
+	"time"
 
+	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/limbo/url_shortener/internal/stats"
 	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 var (
@@ -21,34 +28,114 @@ func TestUpdateStat(t *testing.T) {
 		t.Fatal(err)
 	}
 	cli := stats.NewWithConn(db, "testing")
-	t.Run("successful with no insertion", func(t *testing.T) {
-		mock.ExpectBegin()
-		mock.ExpectExec(`ALTER TABLE`).WithArgs(code).WillReturnResult(sqlmock.NewResult(1, 1))
-		mock.ExpectCommit()
-		err := cli.IncreaseClicks(link, code)
-		assert.NoError(t, err)
-	})
-	t.Run("successful with insertion", func(t *testing.T) {
-		mock.ExpectBegin()
-		mock.ExpectExec(`ALTER TABLE`).WithArgs(code).WillReturnResult(sqlmock.NewResult(0, 0))
-		mock.ExpectExec(`INSERT INTO`).WithArgs(link, code).WillReturnResult(sqlmock.NewResult(1, 1))
-		mock.ExpectCommit()
+	expectedQuery := regexp.QuoteMeta(`INSERT INTO testing.redirect_stat (link, code, clicks) 
+VALUES (?, ?, 1);`)
+	t.Run("successful", func(t *testing.T) {
+		mock.ExpectExec(expectedQuery).WithArgs(link, code).WillReturnResult(sqlmock.NewResult(1, 1))
 		err := cli.IncreaseClicks(link, code)
 		assert.NoError(t, err)
 	})
 	t.Run("error", func(t *testing.T) {
-		mock.ExpectBegin()
-		mock.ExpectExec(`ALTER TABLE`).WithArgs(code).WillReturnError(errors.New("test error"))
-		mock.ExpectRollback()
+		mock.ExpectExec(expectedQuery).WithArgs(link, code).WillReturnError(errors.New("test error"))
 		err := cli.IncreaseClicks(link, code)
 		assert.Error(t, err)
 	})
 }
 
 func TestGetStat(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	cli := stats.NewWithConn(db, "testing")
+	t.Run("successful", func(t *testing.T) {
+		expectedSQL := regexp.QuoteMeta(`SELECT link, code, sum(clicks) FROM testing.redirect_stat WHERE code = ? GROUP BY link, code;`)
+		mock.ExpectQuery(expectedSQL).
+			WithArgs(code).
+			WillReturnRows(sqlmock.NewRows([]string{"link", "code", "clicks"}).AddRow(link, code, clicks))
+		stat, err := cli.GetStats(link, code)
+		assert.NoError(t, err)
+		assert.Equal(t, &stats.ClicksStat{
+			Code:   code,
+			OGLink: link,
+			Clicks: uint64(clicks),
+		}, stat)
+	})
 
+	t.Run("error", func(t *testing.T) {
+		mock.ExpectQuery(`SELECT link, code, clicks`).
+			WithArgs(code).
+			WillReturnError(errors.New("test error"))
+		_, err := cli.GetStats(link, code)
+		assert.Error(t, err)
+	})
 }
 
 func TestIntegrational(t *testing.T) {
+	conn := setupContainer(t)
+	cli := stats.NewWithConn(conn, "default")
+	err := cli.IncreaseClicks(link, code)
+	assert.NoError(t, err)
+	stat, err := cli.GetStats(link, code)
+	assert.NoError(t, err)
+	assert.Equal(t, stats.ClicksStat{
+		OGLink: link,
+		Code:   code,
+		Clicks: 1,
+	}, *stat)
+}
 
+func setupContainer(t *testing.T) *sql.DB {
+	req := testcontainers.ContainerRequest{
+		Image:        "clickhouse/clickhouse-server:23.8",
+		ExposedPorts: []string{"9000/tcp"},
+		Env: map[string]string{
+			"CLICKHOUSE_DB":       "default",
+			"CLICKHOUSE_USER":     "default",
+			"CLICKHOUSE_PASSWORD": "password",
+		},
+		WaitingFor: wait.ForListeningPort("9000/tcp").WithStartupTimeout(30 * time.Second),
+	}
+	container, err := testcontainers.GenericContainer(context.Background(), testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	host, err := container.Host(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	port, err := container.MappedPort(context.Background(), "9000")
+	if err != nil {
+		t.Fatal(err)
+	}
+	db := clickhouse.OpenDB(&clickhouse.Options{
+		Addr: []string{host + ":" + port.Port()},
+		Auth: clickhouse.Auth{
+			Database: "default",
+			Username: "default",
+			Password: "password",
+		},
+	})
+	for i := 0; i < 10; i++ {
+		err := db.Ping()
+		if err == nil {
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS default.redirect_stat (
+    link String, 
+    code String,
+    clicks UInt64
+) ENGINE = SummingMergeTree() ORDER BY clicks;`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		container.Terminate(context.Background())
+	})
+	return db
 }
--- internal/stats/stats.go
+++ internal/stats/stats.go
@@ -4,14 +4,12 @@ import (
 	"database/sql"
 	"errors"
 	"log"
-	"sync"
 	"time"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
 )
 
 type ClicksDBCli struct {
-	txmu   sync.Mutex
 	conn   *sql.DB
 	dbName string
 }
@@ -46,7 +44,6 @@ func New(cfg ClicksDBCfg) *ClicksDBCli {
 	conn.SetMaxIdleConns(10)
 	return &ClicksDBCli{
 		conn:   conn,
-		txmu:   sync.Mutex{},
 		dbName: cfg.Database,
 	}
 }
@@ -54,40 +51,22 @@ func New(cfg ClicksDBCfg) *ClicksDBCli {
 func NewWithConn(conn *sql.DB, dbName string) *ClicksDBCli {
 	return &ClicksDBCli{
 		conn:   conn,
-		txmu:   sync.Mutex{},
 		dbName: dbName,
 	}
 }
 
 func (cli *ClicksDBCli) IncreaseClicks(link, code string) error {
-	cli.txmu.Lock()
-	defer cli.txmu.Unlock()
-	tx, err := cli.conn.Begin()
-	if err != nil {
-		return errors.New("tx error: " + err.Error())
-	}
-	defer tx.Rollback()
-	result, err := tx.Exec(`ALTER TABLE `+cli.dbName+`.redirect_stat UPDATE clicks += 1 WHERE code = ?;`, code)
+	_, err := cli.conn.Exec(`INSERT INTO `+cli.dbName+`.redirect_stat (link, code, clicks) 
+VALUES (?, ?, 1);`, link, code)
 	if err != nil {
 		return errors.New("updating clicks error: " + err.Error())
-	}
-	rowsAffected, _ := result.RowsAffected()
-	if rowsAffected == 0 {
-		_, err = tx.Exec(`INSERT INTO `+cli.dbName+`.redirect_stat (link, code, clicks) VALUES (?, ?, 1);`, link, code)
-		if err != nil {
-			return errors.New("inserting stat error: " + err.Error())
-		}
-	}
-	err = tx.Commit()
-	if err != nil {
-		return errors.New("commiting error: " + err.Error())
 	}
 	return nil
 }
 
 func (cli *ClicksDBCli) GetStats(link, code string) (*ClicksStat, error) {
 	var stat ClicksStat
-	row := cli.conn.QueryRow(`SELECT link, code, clicks FROM `+cli.dbName+`.redirect_stat WHERE code = ?;`, code)
+	row := cli.conn.QueryRow(`SELECT link, code, sum(clicks) FROM `+cli.dbName+`.redirect_stat WHERE code = ? GROUP BY link, code;`, code)
 	if err := row.Scan(&stat.OGLink, &stat.Code, &stat.Clicks); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return &ClicksStat{
