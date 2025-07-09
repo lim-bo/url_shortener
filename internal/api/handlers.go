78c180aed757e9cd5dda3cb6605976ed52918cc0
lim-bo <artemshap66@gmail.com>
2025-07-09 13:08:12 +0300

    fix: CORS middleware fix for proccessing preflight requests

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -16,8 +16,14 @@ import (
 
 func (s *Server) CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
